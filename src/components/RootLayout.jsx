7fd20252dfb6f009a8dc4d390d3b7f9d419bcdd6
xZaaaM <25784985+KiMoOxD@users.noreply.github.com>
2024-10-09 03:47:58 +0300

    updates

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -4,11 +4,13 @@ import Header from "./Header/Header";
 import ScrollToTop from "./ScrollToTop";
 import Footer from "./Footer";
 import { useAllContext } from "../context/AllContext";
+import InitialLoader from "./InitialLoader";
 
 export default function RootLayout() {
   let {lang} = useAllContext()
   return (
     <>
+      <InitialLoader />
       <Header />
       <ScrollToTop />
       <div className={`relative mx-auto ${lang === 'ar' && 'arabic'}`}>

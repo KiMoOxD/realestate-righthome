a1d53cc55f7bad991b4b76bc6c8a4bbcbffced50
KiMoOxD <25784985+KiMoOxD@users.noreply.github.com>
2024-12-24 12:01:18 +0200

    Updates on LazyLoading The Pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import RootLayout from "./components/RootLayout";
 import Home from "./Pages/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AllContextProvider from "./context/AllContext";
-import BrowsePage from "./Pages/BrowsePage";
-import PropertyPage from "./Pages/PropertyPage";
+// import BrowsePage from "./Pages/BrowsePage";
+// import PropertyPage from "./Pages/PropertyPage";
 import { loader } from "./Pages/PropertyPage";
 // import AdminPage from './Pages/AdminPage'
 import ErrorPage from "./components/ErrorPage";
 const LazyAdmin = React.lazy(() => import('./Pages/AdminPage'))
+const LazyPropertyPage = React.lazy(() => import('./Pages/PropertyPage'))
+const LazyBrowsePage = React.lazy(() => import('./Pages/BrowsePage'))
+
 
 const router = createBrowserRouter([
   {
@@ -18,9 +21,9 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Home /> },
-      { path: "browse", element: <BrowsePage />},
-      { path: '/browse/:collection', element: <PropertyPage />, loader: loader},
-      { path: '/browse/:collection/:id', element: <PropertyPage />, loader: loader},
+      { path: "browse", element: <React.Suspense fallback="Loading..."><LazyBrowsePage /></React.Suspense>},
+      // { path: '/browse/:collection', element: <React.Suspense fallback="Loading..."><LazyPropertyPage /></React.Suspense>, loader: loader},
+      { path: '/browse/:collection/:id', element: <React.Suspense fallback="Loading..."><LazyPropertyPage /></React.Suspense>, loader: loader},
       { path: "/admin", element: <React.Suspense fallback="Loading..."><LazyAdmin /></React.Suspense> },
     ],
   },
